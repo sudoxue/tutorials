2cddea2c1fa30d15a60cc53923232297320bfda4
Anton Putra <me@antonputra.com>
2021-09-07 08:48:16 -0700

    tmp

--- lessons/084/gen_client_profile.sh
+++ lessons/084/gen_client_profile.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-KEY_DIR=~/client-configs/keys
-OUTPUT_DIR=~/client-configs/files
-BASE_CONFIG=~/client-configs/base.conf
+KEY_DIR=/etc/openvpn/easy-rsa
+OUTPUT_DIR=/etc/openvpn/client-configs/
+BASE_CONFIG=/etc/openvpn/client-configs/base.ovpn
 
 cat ${BASE_CONFIG} \
     <(echo -e '<ca>') \
-    ${KEY_DIR}/ca.crt \
+    ${KEY_DIR}/pki/ca.crt \
     <(echo -e '</ca>\n<cert>') \
-    ${KEY_DIR}/${1}.crt \
+    ${KEY_DIR}/pki/issued/${1}.crt \
     <(echo -e '</cert>\n<key>') \
-    ${KEY_DIR}/${1}.key \
+    ${KEY_DIR}/pki/private/${1}.key \
     <(echo -e '</key>\n<tls-crypt>') \
     ${KEY_DIR}/ta.key \
     <(echo -e '</tls-crypt>') \
